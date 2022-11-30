47841b397b5718b02958453856c7e2fc0f1dffdc
Andrei Shvetsov <shved963@gmail.com>
2022-11-30 18:49:46 +0300

    Example01_ready

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+Console.Clear();
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+if (!isParsed)
+{
+    Console.WriteLine("Введите корректные данные");
+    return;
+}
+
+if (number < 0)
+{
+    number = -number;
+}
+
+if (number > 99 & number < 1000)
+{
+    int firstNumber = number / 100;
+    int result1 = number / 10;
+    int result2 = result1 - firstNumber*10;
+    Console.WriteLine($"Вторая цирфа числа {number} -> {result2}");
+}
+else
+{
+    Console.WriteLine("Введите корректные данные");
+}
